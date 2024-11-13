b1409014ef562b71ba6b5a9f63a6ae1dc1a9a52a
Simran Bhandari <simranbhandari036@gmail.com>
2024-11-13 16:50:46 +0530

    Creating pty process for terminal

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ const http = require('http')
 const express = require('express')
 const { Server: SocketServer } = require('socket.io')
 const cors = require('cors')
+var pty = require('node-pty');
+
+var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+var ptyProcess = pty.spawn(shell, [], {
+  name: 'xterm-color',
+  cols: 80,
+  rows: 30,
+  cwd: process.env.INIT_CWD,
+  env: process.env
+});
+
 
 const app = express()
 const server = http.createServer(app);
@@ -13,8 +25,18 @@ app.use(cors())
 
 io.attach(server);
 
+ptyProcess.onData(data => {
+    io.emit('terminal:data', data)
+})
+
+
 io.on('connection', (socket) => {
     console.log(`Socket connected`, socket.id)
+
+    socket.on('terminal:write', (data) => {
+        console.log('Term', data)
+        ptyProcess.write(data);
+    })
 })
 
 server.listen(9000, () => console.log(`🐳 Docker server running on port 9000`))
